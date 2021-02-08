6b90757e93654d26ce21d78f9e3783e0f497b0db
Mercedes <mermfb@gmail.com>
2021-02-08 19:20:50 +0100

    Add getDataFromApi function

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
 "use strict";
+
+//CONSTANTES
+const formElement = document.querySelector(".form");
+const inputElement = document.querySelector(".input");
+const buttonElement = document.querySelector(".button");
+let shows = [];
+
+//SEARCH
+
+function getDataFromApi() {
+  const inputValue = inputElement.value;
+  const url = `http://api.tvmaze.com/search/shows?q=${inputValue}`;
+
+  console.log(inputValue);
+
+  event.preventDefault(); //el preventdefault tiene que estar aquí? Por qué?
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      shows = data;
+      console.log(shows);
+      //añadimos llamada a función de pintar
+      //
+    });
+}
+
+//PAINT
+
+//constante para estructura //innerHTML o DOMavanzado
+
+buttonElement.addEventListener("click", getDataFromApi);
