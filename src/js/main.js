05ab6ab92cc2c3f85ac8226d10bcea864cfe6fc0
Mercedes <mermfb@gmail.com>
2021-02-09 17:11:57 +0100

    Add paint for favorites shows

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,64 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+/* eslint-disable no-console */
 "use strict";
 
 //CONSTANTES
 const formElement = document.querySelector(".form");
 const inputElement = document.querySelector(".input");
 const buttonElement = document.querySelector(".button");
-let shows = [];
 const resultSectionElement = document.querySelector(".resultsection");
+let shows = [];
+let favorites = [
+  {
+    score: 17.7553,
+    show: {
+      id: 139,
+      url: "http://www.tvmaze.com/shows/139/girls",
+      name: "Girls",
+      type: "Scripted",
+      language: "English",
+      genres: ["Drama", "Romance"],
+      status: "Ended",
+      runtime: 30,
+      premiered: "2012-04-15",
+      officialSite: "http://www.hbo.com/girls",
+      schedule: { time: "22:00", days: ["Sunday"] },
+      rating: { average: 6.8 },
+      weight: 92,
+      network: {
+        id: 8,
+        name: "HBO",
+        country: {
+          name: "United States",
+          code: "US",
+          timezone: "America/New_York",
+        },
+      },
+      webChannel: null,
+      externals: { tvrage: 30124, thetvdb: 220411, imdb: "tt1723816" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/31/78286.jpg",
+      },
+      summary:
+        "<p>This Emmy winning series is a comic look at the assorted humiliations and rare triumphs of a group of girls in their 20s.</p>",
+      updated: 1611310521,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/139" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/1079686" },
+      },
+    },
+  },
+];
 
 //SEARCH
 
 function getDataFromApi() {
   const inputValue = inputElement.value; //no se puede sacar!!!!
   const url = `http://api.tvmaze.com/search/shows?q=${inputValue}`;
-
-  console.log(inputValue);
 
   event.preventDefault(); //el preventdefault tiene que estar aquí? Por qué?
   fetch(url)
@@ -25,12 +70,10 @@ function getDataFromApi() {
     });
 }
 
-//PAINT  //FALTA COMPROBAR FAVORITOS
+//PAINT
 
-//constante para estructura //innerHTML o DOMavanzado
-//(crear ul con lis dentro de la section) (el ul solo una vez, el li tantas como resultados)
-//h3 para el titulo e imagen
 const list = document.createElement("ul");
+list.setAttribute("class", "js-showlist");
 resultSectionElement.appendChild(list);
 
 function paintShows() {
@@ -43,10 +86,17 @@ function paintShows() {
   //   }
 
   for (let show of shows) {
+    let isFavoriteClass;
+    if (isFavoriteShow(show)) {
+      isFavoriteClass = "show--favorite";
+    } else {
+      isFavoriteClass = "";
+    }
+
     const id = show.show.id;
     const name = show.show.name;
     const listItem = document.createElement("li");
-    listItem.setAttribute("class", "js-show");
+    listItem.setAttribute("class", isFavoriteClass);
     listItem.setAttribute("id", id);
     list.appendChild(listItem);
     const itemTitle = document.createElement("h3");
@@ -66,28 +116,28 @@ function paintShows() {
 
     console.log(listItem);
   }
-  listenShowsEvents();
+  listenShowsEvents(); //llamamos siempre después de pintar
 }
 
-//escuchar shows pintados
+function isFavoriteShow(show) {
+  // compruebo si la paleta que recibo por parámetro está en los favoritos
+  const favoriteFound = favorites.find((favorite) => {
+    // la dificultad de esta función interna del find es saber que tengo que comparar
+    // yo consolearía console.log(favorite, palette) para ver los datos que debo comparar
+    return favorite.show.id === show.show.id;
+  });
+  // find devuelve undefined si no lo encuentra
+  // retorno si está o no está en favoritos
+  if (favoriteFound === undefined) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 //FAVORITES
 
-let favorites = [
-  {
-    externals: { tvrage: null, thetvdb: 357527, imdb: "tt7555294" },
-    genres: (3)[("Drama", "Action", "Crime")],
-    id: 38565,
-    image: {
-      medium:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/260/650446.jpg",
-      original:
-        "http://static.tvmaze.com/uploads/images/original_untouched/260/650446.jpg",
-    },
-    language: "English",
-    name: "L.A.'s Finest",
-  },
-];
+//escuchar shows pintados
 
 function listenShowsEvents() {
   const ShowElements = document.querySelectorAll(".js-show");
@@ -95,8 +145,8 @@ function listenShowsEvents() {
   for (let ShowElement of ShowElements) {
     ShowElement.addEventListener("click", handleShow);
   }
-  saveInLocalStorage();
-  getFromLocalStorage();
+  // saveInLocalStorage();
+  // getFromLocalStorage();
 }
 
 // está función comprueba si la paleta clickada está dentro de favoritos
@@ -105,16 +155,16 @@ function listenShowsEvents() {
 function handleShow(ev) {
   // obtengo el id de la paleta clickada
   const clickedShowId = ev.currentTarget.id;
-  //   const favoritesFoundIndex = favorites.findIndex(function (favorite) {
-  //     return favorite.id === clickedShowId;
-  //   });
-  //   console.log("id del show clickado", clickedShowId);
-  //   const showFound = shows.find(function (show) {
-  //     return show.show.id === clickedShowId;
-  //   });
-  //   // para luego añadirlo al array de favoritos
-  //   favorites.push(showFound);
-  console.log(favorites);
+  const favoritesFoundIndex = favorites.findIndex(function (favorite) {
+    return favorite.id === clickedShowId;
+  });
+  console.log("id del show clickado", clickedShowId);
+  const showFound = shows.find(function (show) {
+    return show.show.id === clickedShowId;
+  });
+  // para luego añadirlo al array de favoritos
+  favorites.push(showFound);
+  console.log("Mis favoritas", favorites);
   paintShows();
 }
 
@@ -139,6 +189,6 @@ function getFromLocalStorage() {
     console.log(favorites);
   }
 }
-console.log(favorites);
+console.log("Mis favoritas", favorites);
 
 buttonElement.addEventListener("click", getDataFromApi);
