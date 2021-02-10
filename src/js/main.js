b1fb903e2de2a969452f50b712da3cfbecf248ab
Mercedes <mermfb@gmail.com>
2021-02-10 07:58:59 +0100

    Add localstorage corrections

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const formElement = document.querySelector(".form");
 const inputElement = document.querySelector(".input");
 const buttonElement = document.querySelector(".button");
 const resultSectionElement = document.querySelector(".resultsection");
+const favoritesSectionElement = document.querySelector(".favoritessection");
 let shows = [];
 let favorites = [
   {
@@ -52,6 +53,48 @@ let favorites = [
       },
     },
   },
+  {
+    score: 13.419667,
+    show: {
+      id: 722,
+      url: "http://www.tvmaze.com/shows/722/the-golden-girls",
+      name: "The Golden Girls",
+      type: "Scripted",
+      language: "English",
+      genres: ["Drama", "Comedy"],
+      status: "Ended",
+      runtime: 30,
+      premiered: "1985-09-14",
+      officialSite: null,
+      schedule: { time: "21:00", days: ["Saturday"] },
+      rating: { average: 8.4 },
+      weight: 94,
+      network: {
+        id: 1,
+        name: "NBC",
+        country: {
+          name: "United States",
+          code: "US",
+          timezone: "America/New_York",
+        },
+      },
+      webChannel: null,
+      externals: { tvrage: 5820, thetvdb: 71292, imdb: "tt0088526" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/6/15097.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/6/15097.jpg",
+      },
+      summary:
+        "<p><b>The Golden Girls</b> follows four South Florida seniors sharing a house, their dreams, and a whole lot of cheesecake. Bright, promiscuous, clueless, and hilarious, these lovely mismatched ladies form the perfect circle of friends.</p>",
+      updated: 1610225849,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/722" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/64040" },
+      },
+    },
+  },
 ];
 
 //SEARCH
@@ -70,7 +113,39 @@ function getDataFromApi() {
     });
 }
 
-//PAINT
+//PAINT FAVORITES
+function paintFavorites() {
+  const favoritesList = document.createElement("ul");
+  favoritesSectionElement.appendChild(favoritesList);
+  for (const favorite of favorites) {
+    let isFavoriteClass;
+    const id = favorite.show.id;
+    const name = favorite.show.name;
+    isFavoriteClass = "show--favorite";
+    const favoriteItem = document.createElement("li");
+    favoriteItem.setAttribute("class", "favoriteItem");
+    favoriteItem.setAttribute("id", id);
+    favoritesList.appendChild(favoriteItem);
+    const itemTitle = document.createElement("h3");
+    let titleContent = document.createTextNode(name);
+    itemTitle.appendChild(titleContent);
+    favoriteItem.appendChild(itemTitle);
+    let itemImage = document.createElement("img");
+    if (favorite.show.image === null) {
+      itemImage.src = `https://via.placeholder.com/210x295/ffffff/666666/?
+      text=TV`;
+    } else if (favorite.show.image.medium === null) {
+      itemImage.src = favorite.show.image.original;
+    } else {
+      itemImage.src = favorite.show.image.medium;
+    }
+    favoriteItem.appendChild(itemImage);
+  }
+}
+
+//más adelante habrá que escuchar las paletas o el botón X para sacar de favoritos
+
+//PAINT SHOWS
 
 const list = document.createElement("ul");
 list.setAttribute("class", "js-showlist");
@@ -79,24 +154,15 @@ resultSectionElement.appendChild(list);
 function paintShows() {
   list.innerHTML = ""; //REVISAR mezclando DOM avanzado con innerHTML!!!
 
-  //   for (let child of list.children) {
-  //     if (child.tagName === "LI") {
-  //       list.removeChild(child);
-  //     }
-  //   }
-
   for (let show of shows) {
     let isFavoriteClass;
     if (isFavoriteShow(show)) {
       isFavoriteClass = "show--favorite";
-    } else {
-      isFavoriteClass = "";
     }
-
     const id = show.show.id;
     const name = show.show.name;
     const listItem = document.createElement("li");
-    listItem.setAttribute("class", isFavoriteClass);
+    listItem.setAttribute("class", isFavoriteClass + " " + "js-show");
     listItem.setAttribute("id", id);
     list.appendChild(listItem);
     const itemTitle = document.createElement("h3");
@@ -120,12 +186,17 @@ function paintShows() {
 }
 
 function isFavoriteShow(show) {
-  // compruebo si la paleta que recibo por parámetro está en los favoritos
+  // compruebo si el show que recibo por parámetro está en los favoritos
   const favoriteFound = favorites.find((favorite) => {
+    console.log("dentro del find", show.show.id);
     // la dificultad de esta función interna del find es saber que tengo que comparar
     // yo consolearía console.log(favorite, palette) para ver los datos que debo comparar
+    console.log("favorite", favorite, "show", show);
+
     return favorite.show.id === show.show.id;
   });
+
+  console.log("show encontrado", favoriteFound, show.show.id);
   // find devuelve undefined si no lo encuentra
   // retorno si está o no está en favoritos
   if (favoriteFound === undefined) {
@@ -145,23 +216,23 @@ function listenShowsEvents() {
   for (let ShowElement of ShowElements) {
     ShowElement.addEventListener("click", handleShow);
   }
-  // saveInLocalStorage();
-  // getFromLocalStorage();
+  saveInLocalStorage();
 }
 
-// está función comprueba si la paleta clickada está dentro de favoritos
-// - Si está lo debo sacar del array de favoritos
-// - Si no está lo debo añadir al array de favoritos
+//comprobar si el show está en favoritos, si no meterlo
 function handleShow(ev) {
-  // obtengo el id de la paleta clickada
+  // obtengo el id del clickado
   const clickedShowId = ev.currentTarget.id;
-  const favoritesFoundIndex = favorites.findIndex(function (favorite) {
-    return favorite.id === clickedShowId;
-  });
   console.log("id del show clickado", clickedShowId);
+  console.log(favorites.id);
+  const favoritesFoundIndex = favorites.findIndex(function (favorite) {
+    return favorite.show.id === clickedShowId;
+  });
   const showFound = shows.find(function (show) {
     return show.show.id === clickedShowId;
   });
+
+  console.log(showFound);
   // para luego añadirlo al array de favoritos
   favorites.push(showFound);
   console.log("Mis favoritas", favorites);
@@ -182,9 +253,8 @@ function getFromLocalStorage() {
   // es decir si es la primera vez que entro en la página
   if (localStorageFavorites !== null) {
     const arrayFavorites = JSON.parse(localStorageFavorites);
-    // lo guardo en la variable global de palettes
     favorites = arrayFavorites;
-    // cada vez que modifico los arrays de palettes o de favorites vuelvo a pintar y a escuchar eventos
+    // cada vez que modifico el array favorites vuelvo a pintar y a escuchar eventos
     paintShows();
     console.log(favorites);
   }
@@ -192,3 +262,5 @@ function getFromLocalStorage() {
 console.log("Mis favoritas", favorites);
 
 buttonElement.addEventListener("click", getDataFromApi);
+getFromLocalStorage();
+paintFavorites();
